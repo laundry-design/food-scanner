4db3d53a4d1854fea1f0355693502e777d8b6134
Antonio <antonio.mancini@clemps.it>
2024-12-12 17:03:48 +0100

    Add back camera type option to image picker

--- app/index.tsx
+++ app/index.tsx
@@ -24,6 +24,7 @@ export default function AIImageScreen() {
   const handleTakePhoto = async () => {
     const result = await ImagePicker.launchCameraAsync({
       mediaTypes: ["images"],
+      cameraType: ImagePicker.CameraType.back,
       quality: 1,
     });
 
